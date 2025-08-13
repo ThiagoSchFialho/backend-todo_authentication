fb2b2dbbdf1e45610161ee95c9311e01f0e371f8
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2025-08-13 06:45:45 -0300

    feat: task routes

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -5,23 +5,72 @@ const { TaskModel } = require('../models/task.model.js');
 const taskModel = new TaskModel();
 
 router.post('/', async function(req, res, next) {
-  const task = await taskModel.createTask("Limpar a casa", "2025-08-15", "07:00", "Aspirar e arrumar a casa", "Casa", 1);
-  res.json(task);
+  const { title, date, time, description, category } = req.body;
+  const { user_id } = req.query;
+
+  try {
+    if (!user_id) return res.status(400).json({ error: 'Usuário indefinido' });
+    if (!title || !date ) return res.status(400).json({ error: 'Campos obrigatórios ausentes' });
+    
+    const task = await taskModel.createTask(title, date, time, description, category, user_id);
+    res.status(201).json(task);
+
+  } catch (error) {
+    console.error('Falha ao criar tarefa', error);
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+  }
 });
 
 router.get('/', async function(req, res, next) {
-  const tasks = await taskModel.getTasks(1);
-  res.json(tasks);
+  const { user_id } = req.query;
+
+  try {
+    if (!user_id) return res.status(400).json({ error: 'Usuário indefinido' });
+
+    const tasks = await taskModel.getTasks(user_id);
+    res.status(200).json(tasks);
+
+  } catch (error) {
+    console.error('Falha ao recuperar tarefas', error);
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+  }
 });
 
 router.put('/', async function(req, res, next) {
-  const task = await taskModel.updateTask(3, "Lavar a louça", "2025-08-15", "08:00", "", "Casa", 1);
-  res.json(task);
+  const { id, title, date, time, description, category } = req.body;
+  const { user_id } = req.query;
+
+  try {
+    if (!user_id) return res.status(400).json({ error: 'Usuário indefinido' });
+
+    const task = await taskModel.updateTask(id, title, date, time, description, category, user_id);
+    if (!task) return res.status(404).json({ error: 'Tarefa não encontrada' });
+
+    res.status(200).json(task);
+
+  } catch (error) {
+    console.error('Falha ao atualizar tarefa', error);
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+  }
 });
 
 router.delete('/', async function(req, res, next) {
-  const task = taskModel.deleteTask(1, 1);
-  res.json(task);
-})
+  const { id } = req.body;
+  const { user_id } = req.query;
+
+  try{
+    if (!user_id) return res.status(400).json({ error: 'Usuário indefinido' });
+    if (!id) return res.status(400).json({ error: 'ID da tarefa não informado' });
+
+    const task = await taskModel.deleteTask(id, user_id);
+    if (!task) return res.status(404).json({ error: 'Tarefa não encontrada' });
+
+    res.status(200).json(task);
+
+  } catch (error) {
+    console.error('Falha ao exluir tarefa', error);
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
 
 module.exports = router;
